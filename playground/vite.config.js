df82d9110f5e8469ee4f08e9d0ad244508b8ae87
hemanta-gayen <hematagayan2000@gmail.com>
2026-08-19 23:41:03 +0800

    fixed deploy-playground.yml

--- playground/vite.config.js
+++ playground/vite.config.js
@@ -4,6 +4,7 @@ import { defineConfig } from "vite"
 export default defineConfig({
   root: fileURLToPath(new URL(".", import.meta.url)),
   publicDir: fileURLToPath(new URL("../resources", import.meta.url)),
+  base: "./",
   build: {
     outDir: "dist",
     emptyOutDir: true
